46c5b925db36d4b86a56285fd3a3ddc92a41e806
sabbir-dcy <sabbirahmed.sa08@gmail.com>
2022-12-14 15:30:50 +0600

    sec

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
+const app = express();
+const port = process.env.PORT || 300;
+app.use(express.json());
+
+app.listen(port, () => console.log("listening to the app", port));
+app.get("/", async (req, res) => res.json({ hello: "test" }));
