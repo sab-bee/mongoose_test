c11d30910f4fbd8d9cb86974bf42abf5113c97c8
sabbir-dcy <sabbirahmed.sa08@gmail.com>
2022-12-14 15:44:40 +0600

    sec

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ const app = express();
 const port = process.env.PORT || 300;
 app.use(express.json());
 
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9krdwpd.mongodb.net/my_db?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(url || "mongodb://127.0.0.1:27017/uni_insurance", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log(err));
+
 app.listen(port, () => console.log("listening to the app", port));
 app.get("/", async (req, res) => res.json({ hello: "test" }));
 
-app.use('/data', dataHandler)
+app.use("/data", dataHandler);
--- routeHandler/dataHandler.js
+++ routeHandler/dataHandler.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const dataSchema = require("../Schema/dataSchema");
 
-
 const Data = new mongoose.model("Data", dataSchema);
 
 router.get("/", async (req, res) => {
-  res.send({ data: "data" });
+  const result = await Data.find({});
+  res.send(result);
 });
 
 module.exports = router;
