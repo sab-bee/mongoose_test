f8d0a8df19562a6c21bb9320c52a2c8df4c5c5c9
sabbir-dcy <sabbirahmed.sa08@gmail.com>
2022-12-17 02:54:24 +0600

    code check

--- routeHandler/subscriptionHandler.js
+++ routeHandler/subscriptionHandler.js
@@ -4,11 +4,11 @@ const mongoose = require("mongoose");
 const subscriptionSchema = require("../Schema/subscriptionSchema");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const verifyUser = require("../middleware/verifyUser");
-
+const cors = require('cors')
 const router = express.Router();
 const Subscription = new mongoose.model("Subscription", subscriptionSchema);
 
-router.get("/payment/:premium", verifyUser, async (req, res) => {
+router.get("/payment/:premium", cors(), verifyUser, async (req, res) => {
   const premium = req.params.premium * 100;
   const paymentIntents = await stripe.paymentIntents.create({
     amount: premium,
