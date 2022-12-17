f235bc3bc5ab4dc2a4b46a99d731dd5b2d9d2a1b
sabbir-dcy <sabbirahmed.sa08@gmail.com>
2022-12-17 13:28:23 +0600

    tiral 9

--- routeHandler/subscriptionHandler.js
+++ routeHandler/subscriptionHandler.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const subscriptionSchema = require("../Schema/subscriptionSchema");
@@ -8,20 +7,23 @@ const verifyUser = require("../middleware/verifyUser");
 const router = express.Router();
 const Subscription = new mongoose.model("Subscription", subscriptionSchema);
 
-router.options('/payment/:premium', cors())
-router.get(
-  "/payment/:premium",
-  verifyUser,
-  cors(),
-  async (req, res, next) => {
-    const premium = req.params.premium * 100;
-    const paymentIntents = await stripe.paymentIntents.create({
-      amount: premium,
-      currency: "usd",
-      payment_method_types: ["card"],
-    });
-    res.send({ clientSecret: paymentIntents.client_secret });
-  }
-);
+// router.options("/payment/:premium", cors());
+router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+router.get("/payment/:premium", verifyUser, async (req, res) => {
+  const premium = req.params.premium * 100;
+  const paymentIntents = await stripe.paymentIntents.create({
+    amount: premium,
+    currency: "usd",
+    payment_method_types: ["card"],
+  });
+  res.send({ clientSecret: paymentIntents.client_secret });
+});
 
 module.exports = router;
